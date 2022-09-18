cf02de156271fb2785f8df8bcb65ffc60bebc911
oksalikes <109412597+oksalikes@users.noreply.github.com>
2022-09-18 22:22:17 +0300

    Create program.cs

---
+++ program.cs
@@ -0,0 +1,7 @@
+using static System.Console;
+Clear();
+
+
+string[] array1 = new string[5] {"198", "38", "hi", "yes", "world"};
+string[] array2 = new string[array1.Length];
+void SecondArrayWithIF(string[] array1, string[] array2)
